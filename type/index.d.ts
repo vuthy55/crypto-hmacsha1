8db8758ff101730a909f7e69d515370d6442f8af
AndyWang <wangyuanwenqi@outlook.com>
2021-04-07 14:55:50 +0800

    update d.ts

--- type/index.d.ts
+++ type/index.d.ts
@@ -1,7 +1,2 @@
-declare function SHA1(key:string, message:string):string;
-declare function HmacSHA1(key:string, message:string):string;
-
-declare namespace CryptoJS {
-    function SHA1(key:string, message:string):string;
-    function HmacSHA1(key:string, message:string):string;
-}
+export declare function SHA1(key:string, message:string):string;
+export declare function HmacSHA1(key:string, message:string):string;
